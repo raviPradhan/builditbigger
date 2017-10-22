c70d2e822bfda15217dec56cdee3cc88046fd408
Ravi Pradhan <ravi.pradhan@druplets.in>
2017-10-22 13:30:11 +0530

    complete application

--- app/src/androidTestPaid/java/com/udacity/gradle/builditbigger/paid/PaidTester.java
+++ app/src/androidTestPaid/java/com/udacity/gradle/builditbigger/paid/PaidTester.java
@@ -1,4 +1,4 @@
-package com.udacity.gradle.builditbigger.free;
+package com.udacity.gradle.builditbigger.paid;
 
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
--- app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,10 +1,14 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.ravi.jokeviewer.ShowJokeActivity;
 
 /*import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;*/
@@ -37,7 +41,6 @@ public class MainActivityFragment extends Fragment implements EndpointsAsyncTask
 
     @Override
     public void showJoke(String result) {
-        joke = result;
         startActivity(new Intent(getContext(), ShowJokeActivity.class).putExtra("joke", result));
     }
 }
